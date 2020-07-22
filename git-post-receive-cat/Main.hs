c8661f6582654006c626684b61b6716cbd285daa
Richard Marko <srk@48.io>
2020-07-22 12:07:13 +0200

    cat: refactor, use pretty, add basic filtering

--- git-post-receive-cat/Main.hs
+++ git-post-receive-cat/Main.hs
@@ -1,19 +1,48 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards   #-}
 module Main where
 
-import Control.Monad
-import Control.Monad.IO.Class
-import qualified Data.ByteString.Char8 as B
+import Control.Monad (forM_, when)
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.Text
+import qualified Data.Text.Encoding
+import qualified Data.Text.IO
+import qualified Data.ByteString.Char8
 
-import Git.PostReceive
+import Git.PostReceive.Types
+import Git.PostReceive.ZRE
+import Git.PostReceive.Pretty
 import Network.ZRE
 
-group = "commits"
+import Options.Applicative
 
-main = runZre $ do
-  zjoin group
-  zrecvShouts $ whenDecodes postReceiveHookDecode $ \Batch{..} -> do
-    forM_ batchCommits $ \Commit{..} -> do
-      liftIO $ B.putStrLn $ B.unwords
-        [batchRepoName, commitRev]
+data CatOpts = CatOpts {
+    commitsOnly :: Bool
+  , dull        :: Bool
+  , refs        :: [String]
+  , repos       :: [String]
+  } deriving (Show)
+
+parseCatOptions = CatOpts
+   <$> switch (long "commits" <> short 'c')
+   <*> switch (long "dull")
+   <*> many (strOption (long "branch" <> short 'b' <> metavar "BRANCH"))
+   <*> many (strArgument (metavar "REPO"))
+
+main :: IO ()
+main = subscribeZreWith parseCatOptions $ \cfg batch' -> do
+  let batch = case refs cfg of
+        [] -> batch'
+        _  -> filterBatchBranches batch' (refs cfg)
+
+  when (null (repos cfg) || (Data.ByteString.Char8.unpack $ batchRepo batch) `elem` (repos cfg)) $ do
+      liftIO
+        $ Data.Text.IO.putStrLn
+        $ renderBatch
+        $ fmap (Data.Text.Encoding.decodeUtf8)
+        $ batch
+
+-- XXX: filtering needs to go elsewhere
+-- as it's needed by e.g. zre2irc too
+filterBatchBranches batch refs = batch { batchCommits = filter (f refs) (batchCommits batch) }
+  where f rs Commit{..} = (Data.ByteString.Char8.unpack commitBranch) `elem` (map ("heads/"++) rs)
