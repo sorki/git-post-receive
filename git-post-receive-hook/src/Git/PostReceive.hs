7ed53665aa6051d22a7a35df691bcf41309c75de
Richard Marko <srk@48.io>
2020-07-22 12:06:38 +0200

    hook: refactor, better error handling, qualified imports

--- git-post-receive-hook/src/Git/PostReceive.hs
+++ git-post-receive-hook/src/Git/PostReceive.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE FlexibleInstances #-}
+-- {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveFunctor #-}
+-- {-# LANGUAGE DeriveGeneric #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Git.PostReceive (
     module Git.PostReceive.Types
   , postReceiveHook
-  , postReceiveHookEncode
-  , postReceiveHookDecode
   , postReceiveHookShow
   , runInRepo
   , withRepoCheckout
@@ -14,93 +15,118 @@ module Git.PostReceive (
   where
 
 import Data.ByteString (ByteString)
+import Data.Text (Text)
+import Data.Time (UTCTime)
+
 import qualified Data.ByteString.Char8 as B
 import qualified Data.Text             as T
 
+import Control.Applicative
+import qualified Control.Foldl
 import Control.Monad (forM)
-import qualified Control.Foldl as Fold
+import Control.Monad.IO.Class (MonadIO)
 
 import Data.Git hiding (Commit, Tag)
 import Data.Git.Ref (HashAlgorithm, fromHex, toHex)
-import Data.Git.Storage
+import Data.Git.Repository (getCommitMaybe)
 import Data.Git.Storage.Object
+
+import qualified Data.Git
+
+import Data.Hourglass (Elapsed(..), Seconds(..), timeGetElapsed)
 import Data.Serialize
-import Turtle
+import Data.Typeable (Typeable)
+import qualified Turtle
 
 import Git.PostReceive.Types
+import Git.PostReceive.Serialize
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 
-instance Serialize Change
-instance Serialize (Commit   ByteString)
-instance Serialize (Tag      ByteString)
-instance Serialize (LightTag ByteString)
-instance Serialize (Batch    ByteString)
+data PostInfo a = PostInfo {
+    oldRev :: a
+  , newRev :: a
+  , ref    :: a
+  } deriving (Eq, Show, Ord, Functor)
 
-data PostInfo = PostInfo {
-    oldRev :: ByteString
-  , newRev :: ByteString
-  , ref    :: ByteString
-  } deriving (Eq, Show, Ord)
-
-toPostInfo [oldRev, newRev, ref] = PostInfo{..}
-toPostInfo x = error $ "Wrong hook input, expecting 'oldrev newrev ref' but got " ++ show x
-
-infoToChange PostInfo{..} | B.all (=='0') oldRev = Create
-infoToChange PostInfo{..} | B.all (=='0') newRev = Delete
-infoToChange PostInfo{..} | otherwise            = Update
-
+postReceiveHookShow :: IO ByteString
 postReceiveHookShow = B.pack . show <$> postReceiveHook
-postReceiveHookEncode = encode <$> postReceiveHook
 
-postReceiveHookDecode :: B.ByteString -> Either String (Batch ByteString)
-postReceiveHookDecode = decode
+cvtTime :: Elapsed -> UTCTime
+cvtTime (Elapsed (Seconds seconds)) = posixSecondsToUTCTime . fromIntegral $ seconds
 
+postReceiveHook :: IO (Batch ByteString)
 postReceiveHook = do
-  infos <- fold (stdin) $ Fold.premap (toPostInfo . (map (B.pack . T.unpack)) . T.words . lineToText) Fold.list
-  print infos
+  infos <- Turtle.fold Turtle.stdin
+    $ Control.Foldl.premap
+        ( toPostInfo
+        . (map (B.pack . T.unpack))
+        . T.words
+        . Turtle.lineToText
+        )
+        Control.Foldl.list
+
   out <- forM infos $ \postInfo@PostInfo{..} -> do
     withCurrentRepo $ \repo -> do
       mobj <- getObject repo (fromHex newRev) True
       commits <- case mobj of
           Just (ObjCommit c) -> do
-            case (B.stripPrefix "refs/heads/" ref) of
+            case (B.stripPrefix "refs/" ref) of
               Just branch -> do
-                forcePushed <- fmap not <$> fold (inshell (T.unwords [
-                  "git rev-list"
-                  , (T.pack $ B.unpack $ oldRev)
-                  , (T.cons '^' (T.pack $ B.unpack $ newRev))
-                  ]) empty) $ Fold.null
-
-                let mkCommit rev pInfo c forced =
+                let mkCommit rev pInfo cm forced =
                       Commit {
-                        commitRev = rev
-                      , commitBranch = branch
-                      , commitChangeType = infoToChange pInfo
-                      , commitAuthorName = personName  $ commitAuthor c
-                      , commitAuthorMail = personEmail $ commitAuthor c
-                      , commitMsg = commitMessage c
-                      , commitForced = forced
+                        commitRev           = rev
+                      , commitBranch        = branch
+                      , commitChangeType    = infoToChange pInfo
+                      , commitAuthorName    = personName  $ commitAuthor cm
+                      , commitAuthorMail    = personEmail $ commitAuthor cm
+                      , commitAuthored      = cvtTime $ timeGetElapsed $ personTime $ commitAuthor cm
+                      , commitCommitterName = personName $ commitCommitter cm
+                      , commitCommitterMail = personEmail $ commitCommitter cm
+                      , commitCommitted     = cvtTime $ timeGetElapsed $ personTime $ commitCommitter cm
+                      , commitMsg           = commitMessage cm
+                      , commitForced        = forced
                       }
 
-                case forcePushed of
-                  True -> return $ [mkCommit newRev postInfo c True]
-                  False -> do
-                    -- list commit hashes with git log oldRev..newRev
-                    hashes <- fold (
-                      inshell (
-                           "git log --pretty=format:%H"
-                        <> " "
-                        <> (T.pack $ B.unpack $ oldRev)
-                        <> ".."
-                        <> (T.pack $ B.unpack $ newRev)
-                      ) empty) $ Fold.premap
-                        (textToRef . lineToText) Fold.list
+                case infoToChange postInfo of
+                  Create -> return [mkCommit newRev postInfo c False]
+                  Delete -> return [mkCommit oldRev postInfo c False]
+                  Update -> do
+                    forcePushed <- Turtle.fold (Turtle.inshell (T.unwords [
+                      "git rev-list"
+                      , (T.pack $ B.unpack $ oldRev)
+                      , (T.cons '^' (T.pack $ B.unpack $ newRev))
+                      ]) empty) $ fmap not Control.Foldl.null
 
-                    commits <- forM hashes $ \h -> do
-                      c' <- getCommit repo h
-                      return $ mkCommit (toHex h) postInfo c' False
+                    case forcePushed of
+                      True -> return [mkCommit newRev postInfo c True]
+                      False -> do
 
-                    -- in chronological order
-                    return $ reverse commits
+                        -- list commit hashes with git log oldRev..newRev
+                        hashes <- Turtle.fold (
+                          Turtle.inshell
+                            (  "git log --pretty=format:%H"
+                            <> " "
+                            <> (T.pack $ B.unpack $ oldRev)
+                            <> ".."
+                            <> (T.pack $ B.unpack $ newRev)
+                            )
+                            empty)
+                          $ Control.Foldl.premap
+                              (textToRef . Turtle.lineToText)
+                              Control.Foldl.list
+
+                        commits <- forM hashes $ \h -> do
+                          mc' <- getCommitMaybe repo h
+                          case mc' of
+                            Nothing -> do
+                              -- XXX: is this a race condition?
+                              -- XXX: how to handle this better?
+                              error $ show ("getCommit 404", h)
+                              --return []
+                            Just c' -> return $ mkCommit (toHex h) postInfo c' False
+
+                        -- in chronological order
+                        return $ reverse commits
 
               Nothing -> do
                 putStrLn "Wont handle "
@@ -110,7 +136,7 @@ postReceiveHook = do
           _ -> return []
 
       lightTags <- case mobj of
-          Just (ObjCommit c) -> do
+          Just (ObjCommit _c) -> do
             case (B.stripPrefix "refs/tags/" ref) of
               Just name -> do
                 return [
@@ -133,15 +159,29 @@ postReceiveHook = do
 
       return (commits, lightTags, tags)
 
-  x <- pwd
+  x <- Turtle.pwd
   return $ Batch {
-      batchRepo      = (B.pack $ T.unpack $ format fp x)
-    , batchRepoName  = (B.pack $ T.unpack $ format fp (basename x))
+      batchRepo      = (B.pack $ T.unpack $ Turtle.format Turtle.fp x)
+    , batchRepoName  = (B.pack $ T.unpack $ Turtle.format Turtle.fp (Turtle.basename x))
     , batchCommits   = concat $ map (\(c, _, _) -> c) out
     , batchLightTags = concat $ map (\(_, l, _) -> l) out
     , batchTags      = concat $ map (\(_, _, t) -> t) out
     }
 
+toPostInfo :: Show a => [a] -> PostInfo a
+toPostInfo [oldRev, newRev, ref] = PostInfo{..}
+toPostInfo x = error $ "Wrong hook input, expecting 'oldrev newrev ref' but got " ++ show x
+
+infoToChange :: PostInfo ByteString -> Change
+infoToChange PostInfo{..} | B.all (=='0') oldRev = Create
+infoToChange PostInfo{..} | B.all (=='0') newRev = Delete
+infoToChange PostInfo{}   | otherwise            = Update
+
+walk :: (Typeable hash, HashAlgorithm hash)
+     => Git hash
+     -> Text
+     -> Text
+     -> IO [Data.Git.Commit hash]
 walk repo new old = go (textToRef new)
   where
   go rev = do
@@ -159,26 +199,27 @@ textToRef = fromHex . B.pack . T.unpack
 refToText :: HashAlgorithm hash => Ref hash -> Text
 refToText = T.pack . B.unpack . toHex
 
+restorePwd :: MonadIO m => m b -> m b
 restorePwd act = do
-  old <- pwd
+  old <- Turtle.pwd
   ret <- act
-  cd old
+  Turtle.cd old
   return ret
 
 runInRepo :: Batch ByteString -> Text -> IO (Text)
 runInRepo Batch{..} cmd = restorePwd $ do
-  cd (fromText $ T.pack $ B.unpack $ batchRepo)
-  (_retcode, out, err) <- shellStrictWithErr cmd empty
-  return $ T.concat [out, err]
+  Turtle.cd $ Turtle.fromText $ T.pack $ B.unpack $ batchRepo
+  (_retcode, out, errout) <- Turtle.shellStrictWithErr cmd empty
+  return $ T.concat [out, errout]
 
 withRepoCheckout :: Batch ByteString -> IO a -> IO a
 withRepoCheckout Batch{..} cmd = restorePwd $ do
-  with (mktempdir "/tmp" "git") $ \d -> do
-    stdout (inshell
+  Turtle.with (Turtle.mktempdir "/tmp" "git") $ \dir -> do
+    Turtle.stdout (Turtle.inshell
       ("git clone "
       <> (T.pack . B.unpack $ batchRepo)
       <> " "
-      <> (format fp d)) empty)
+      <> (Turtle.format Turtle.fp dir)) empty)
 
-    cd d
+    Turtle.cd dir
     cmd
